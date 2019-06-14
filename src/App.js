1c0a8274770e5a50b3dc3deea7150831be1ed383
Ricardo <ricardoromero4444@gmail.com>
2019-06-14 17:58:24 -0300

    Global.css created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {  // Isto e um componente
   return (
     <div className="App">
-
+      <p>Hello</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './global.css'
+
 ReactDOM.render(<App />, document.getElementById('root'));  // Pega o componente App e coloca dentro da <div> com id 'root'
 
